2817a1ce9d5643416f1700421dfee92923b5ed66
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2016-06-02 23:14:01 +0900

    set z-index

--- src/template.js
+++ src/template.js
@@ -13,7 +13,7 @@ var template = `
       </span>
     </div>
 
-    <div grafana-heatmap-epoch class="epoch-theme-dark epoch category20" ng-dblclick="ctrl.zoomOut()">
+    <div grafana-heatmap-epoch class="epoch-theme-dark epoch category20" style="z-index: 0;" ng-dblclick="ctrl.zoomOut()">
     </div>
 
   </div>
